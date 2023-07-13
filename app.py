19e63f868f729a3237e78a33d384e586d586129c
Moein <moein.mac@gmail.com>
2023-07-13 18:38:38 +0330

    reduce expand Image , Kernel

---
+++ app.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+from scipy import ndimage
+
+kernel = (1.0/256)*np.array([[1, 4, 6, 4, 1],[4, 16, 24, 16, 4],[6, 24, 36, 24, 6], [4, 16, 24, 16, 4],[1, 4, 6, 4, 1]])
+
+def reduceImage(image):
+  imageFilter = cv2.filter2D(image,-1,kernel)
+  reduced = imageFilter[::2, ::2] 
+  return reduced
+
+def expandImage(image):
+  expanded = np.zeros((2*image.shape[0], 2*image.shape[1]))
+  expanded[::2, ::2] = image
+  imageFilter = ndimage.convolve(expanded,4*kernel, mode='constant')
+  return imageFilter
